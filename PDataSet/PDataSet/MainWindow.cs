6011498d42bc4b7dfec613b78c7f4cfe624bcefe
Daniel Mayans <dani.mb90@gmail.com>
2013-01-30 17:24:00 +0100

    actualizando DataSet

--- PDataSet/PDataSet/MainWindow.cs
+++ PDataSet/PDataSet/MainWindow.cs
@@ -2,6 +2,8 @@ using System;
 using System.Data;
 using Npgsql;
 using Gtk;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 public partial class MainWindow: Gtk.Window
 {	
@@ -19,14 +21,42 @@ public partial class MainWindow: Gtk.Window
 	protected void OnExecuteActionActivated (object sender, System.EventArgs e)
 	{
 		string connectionString = "Server=localhost;Database=danimb;User Id=danimb;Password=sistemas"; 
-		IDbConnection dbConnection = new NpgsqlConnection(connectionString);
-		IDbCommand selectCommand = dbConnection.CreateCommand();
+		NpgsqlConnection dbConnection = new NpgsqlConnection(connectionString);
+		NpgsqlCommand selectCommand = dbConnection.CreateCommand();
 		selectCommand.CommandText = "select * from articulo";
-		IDbDataAdapter dbDataAdapter = new NpgsqlDataAdapter();
+		NpgsqlDataAdapter dbDataAdapter = new NpgsqlDataAdapter();
+		new NpgsqlCommandBuilder((NpgsqlDataAdapter)dbDataAdapter);
+		
 		dbDataAdapter.SelectCommand = selectCommand;
+		
 		DataSet dataSet = new DataSet();
 		
-		dbDataAdapter.Fill(dataSet);
-		Console.WriteLine("Tables.Count=(0)", dataSet.Tables.Count);
- 	}
+		dbDataAdapter.Fill (dataSet);
+		
+		Console.WriteLine("Tables.Count={0}, dataSet.Tables.Count");
+		
+		foreach (DataTable dataTable in dataSet.Tables){
+			show (dataTable);
+			
+		DataRow dataRow = dataSet.Tables[0].Rows[0];
+		dataRow["nombre"] = DateTime.Now.ToString();
+			
+		Console.WriteLine("Tabla con los cambios:");
+			show (dataSet.Tables[0]);
+		};                         
+			dbDataAdapter.Update(dataSet.Tables[0]);
+	}
+	
+	private void show(DataTable dataTable){
+//		foreach (DataColumn dataColumn in dataTable.Columns)
+//			Console.WriteLine("Column.Name={0})", dataColumn.ColumnName);
+		
+		foreach (DataRow dataRow in dataTable.Rows){
+			foreach (DataColumn dataColumn in dataTable.Columns)
+				Console.Write("[{0}={1}] ", dataColumn.ColumnName, dataRow[dataColumn]);
+			Console.WriteLine();
+		}
+			
+	
+	}
 }
